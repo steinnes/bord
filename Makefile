df27ce70aff79945412972a91d774b63d35aaaa7
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2020-02-20 00:39:33 +0000

    Very simplistic version working
    - switched to pyppeteer 0.2.2 which has the websocket timeout fix
      .. not sure why it didn't install that version initially
    - added screeninfo and resolution detection for a single monitor
    - start chromium fullscreen (--kiosk) and without the automation warning infobar
    - set viewports to use entire screen

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 black:
 	poetry run black $(shell git diff --name-only HEAD|grep \.py$)
+
+run:
+	poetry run python app.py
--- app.py
+++ app.py
@@ -6,38 +6,45 @@ from typing import List
 
 from dataclasses import dataclass
 
+from screeninfo import get_monitors, Enumerator, Monitor
+
+
 @dataclass(frozen=True)
 class Config:
     interval: int
     urls: List[str]
+    monitor: Monitor
 
 
-def read_config():
+def init_config():
     with open("./config.json") as fp:
         cfg_json = json.load(fp)
-    return Config(**cfg_json)
 
+    monitors = [monitor for monitor in get_monitors(Enumerator.Xinerama)]
+    assert len(monitors) == 1, "Multiple monitors are not supported"
+    return Config(monitor=monitors[0], **cfg_json)
 
 
 async def main():
-    browser = await launch(executablePath='/usr/bin/chromium-browser', headless=False, dumpio=True, logLevel="INFO", autoclose=False)
-    config = read_config()
+    browser = await launch(
+        headless=False, dumpio=True, logLevel="INFO", autoclose=False, args=['--kiosk', '--disable-infobars'],
+    )
+    config = init_config()
     tabs = await browser.pages()
     while len(tabs) < len(config.urls):
         tabs.append(await browser.newPage())
 
     for tab, url in zip(tabs, config.urls):
         print("Set tab:", tab, " to url:", url)
+        await tab.setViewport(dict(width=config.monitor.width, height=config.monitor.height))
         await tab.goto(url, timeout=0)
 
     tab_index = 0
-    import time
+
     while True:
         tab = tabs[tab_index]
         await asyncio.sleep(config.interval)
-        print(time.ctime())
-        print(f"Will show tab[{tab_index}] = {tab}")
-        print("..isClosed=", tab.isClosed())
+        print(f"Switching to tab[{tab_index}] = {tab}")
         await tabs[tab_index].bringToFront()
         tab_index += 1
         if tab_index == len(tabs):
