7e9cfe394694133070c70fef19be0d335a3b92d3
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2020-02-24 11:21:10 +0000

    Add test for executable_path override

--- test/test_config.py
+++ test/test_config.py
@@ -13,15 +13,32 @@ def config_file():
         config = {"interval": 10, "urls": ["https://a.com", "https://b.com"]}
         config = {**config, **overrides}
         with tempfile.NamedTemporaryFile() as fp:
-            fp.write(json.dumps(config).encode('utf-8'))
+            fp.write(json.dumps(config).encode("utf-8"))
             fp.seek(0)
             yield fp.name
+
     return inner
 
 
-def test_config(config_file):
+def test_config_defaults(config_file):
+    # Arrange
     with config_file() as f:
+        # Act
         cfg = init_config(f)
+
+        # Assert
         assert len(cfg.urls) == 2
         assert cfg.interval == 10
         assert cfg.executable_path is None
+
+
+def test_config_overrides(config_file):
+    # Arrange
+    with config_file(interval=1, urls=["https://c.com"], executable_path="/usr/bin/chromium") as f:
+        # Act
+        cfg = init_config(f)
+
+        # Assert
+        assert len(cfg.urls) == 1
+        assert cfg.interval == 1
+        assert cfg.executable_path == "/usr/bin/chromium"
